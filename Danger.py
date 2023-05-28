dfaa43423a0ddbbc40ee8b613b51255dab6df729
rangjin <46679581+rangjin@users.noreply.github.com>
2023-05-28 17:18:16 +0900

    Update Danger.py

--- Danger.py
+++ Danger.py
@@ -4,19 +4,6 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 
 
-class KR(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        # 경고 메시지 박스 표시, 정확한 정부 페이지에 접속했음을 알림
-        QMessageBox.information(
-            self, 'Connect Alert', ' \n \n [Republic of KOREA] \n \n You are in KOREA OFFICIAL PAGE \n'
-        )
-        sys.exit()
-
-
 class PHISHING(QWidget):
     def __init__(self):
         super().__init__()
